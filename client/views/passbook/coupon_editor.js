ef092f51ca037d612aecf68205dbdacb1319f15c
yamironov <yamironov@gmail.com>
2014-03-29 17:32:39 +0400

    New design for passbook editor

--- client/views/passbook/coupon_editor.js
+++ client/views/passbook/coupon_editor.js
@@ -130,45 +130,5 @@ Template.couponEditor.events({
   },
   'change #code': function(e){
     Session.set('couponCode',$(event.target).val());
-  },
-
-  'click #delete': function(e){
-      e.preventDefault();
-      if (confirm("Вы точно хотите удалить этот купон?")){
-        Coupons.remove({_id:this._id}, function(error){
-          if (error){
-              alert(error.reason);
-            }else{
-              Router.go('coupons');
-            }
-        });
-      }
-  },
-
-  'click #save': function(e){
-    e.preventDefault();
-
-    var coupon = {
-      title: Session.get('couponTitle'),
-      coupon: Session.get('couponCoupon'),
-      description: Session.get('couponDescription'),
-      addInfoLabel: Session.get('couponAddInfoLabel'),
-      addInfoValue: Session.get('couponAddInfoValue'),
-      code: Session.get('couponCode'),
-      logo: Session.get('couponLogo'),
-      banner: Session.get('couponBanner')
-    };
-
-    if (this._id){
-      _.extend(coupon,{_id:this._id});
-    }
-
-    Meteor.call('saveCoupon',coupon,function(error){
-      if (error){
-        alert(error.reason);
-      }else{
-        Router.go('coupons');
-      }
-    });
-  },
+  }
 });
