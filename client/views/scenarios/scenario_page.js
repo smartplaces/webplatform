bc1284a2e05b6a4498526d69def5ffc2a32bfee2
Alexander Seliverstov <aseliverstov@smartplaces.ru>
2014-12-04 12:17:33 +0000

    Close #18. Add preview for scenario in editor

--- client/views/scenarios/scenario_page.js
+++ client/views/scenarios/scenario_page.js
@@ -47,9 +47,10 @@ Template.scenarioPage.rendered = function (){
     
     if (this.data){
         Session.set('isNewScenario', false);
-        
+        Session.get('scenario.active',this.data.active);
         Session.set('scenario.name',this.data.name);
-        Session.set('scenario.beacons',this.data.beacons.length);
+        Session.set('scenario.start',this.data.start);
+        Session.set('scenario.end',this.data.end);
         
         $('#beacons').val(this.data.beacons).trigger('change');
         $('#proximity').val(this.data.proximity).trigger('change');
@@ -59,8 +60,16 @@ Template.scenarioPage.rendered = function (){
         $('#frequency').val(this.data.frequency).trigger('change');
     }else{
         Session.set('isNewScenario', true);
-        Session.set('scenario.name',undefined);
+        Session.set('scenario.active',true);
+        Session.set('scenario.start',undefined);
+        Session.set('scenario.end',undefined);
+        Session.set('scenario.name', undefined);
         Session.set('scenario.beacons',undefined);
+        Session.set('scenario.event',$('#event').val());
+        Session.set('scenario.proximity',$('#proximity').val());
+        Session.set('scenario.message',$('#message').val());
+        Session.set('scenario.coupon',undefined);
+        Session.set('scenario.frequency',undefined);
     }
 }
 
@@ -69,8 +78,44 @@ Template.scenarioPage.events({
         Session.set('scenario.name',e.target.value);
     },
     
+    'change #active': function(e,t){
+        Session.set('scenario.active',$(e.target).is(':checked'));
+    },
+    
+    'change #event': function(e,t){
+        Session.set('scenario.event',e.target.value);
+    },
+    
+    'change #proximity': function(e,t){
+        Session.set('scenario.proximity',e.target.value);
+    },
+    
+    'change #message': function(e,t){
+        Session.set('scenario.message',e.target.value);
+    },
+    
+    'change #coupon': function(e,t){
+        Session.set('scenario.coupon',e.target.value);
+    },
+    
+    'change #frequency': function(e,t){
+        Session.set('scenario.frequency',e.target.value);
+    },
+    
     'change #beacons': function(e,t){
-        Session.set('scenario.beacons',e.target.value);
+        Session.set('scenario.beacons',$(e.target).val());
+    },
+    
+    'change #start_dp': function(e,t){
+        var d  = $(e.target).find('[id=start]').val();
+        if (d) d = new Date(d.replace(/(\d{2})\.(\d{2})\.(\d{4})/,'$3-$2-$1'));
+        Session.set('scenario.start',d);        
+    },
+    
+    'change #end_dp': function(e,t){
+        var d  = $(e.target).find('[id=end]').val();
+        if (d) d = new Date(d.replace(/(\d{2})\.(\d{2})\.(\d{4})/,'$3-$2-$1'));
+        Session.set('scenario.end',d);
     },
     
     'click #delete':function(e){
@@ -141,6 +186,10 @@ Template.scenarioPage.helpers({
         return Coupons.find();
     },
     
+    beacons: function(){
+        return Session.get('scenario.beacons');
+    },
+    
     name: function(){
         return Session.get('scenario.name');
     },
@@ -183,5 +232,82 @@ Template.scenarioPage.helpers({
 
     primaryFieldLabel: function(){
         return this.pass.coupon.primaryFields[0].label;
+    },
+    
+    label: function(type){
+        var col = [];
+        var code = ''
+
+        if (type === 'event'){
+            col = EVENTS;
+            code = Session.get('scenario.event');
+        }else if (type === 'proximity'){
+            col = PROXIMITIES;
+            code = Session.get('scenario.proximity');
+        }else if (type === 'frequency'){
+            col = FREQUENCIES;
+            code = Session.get('scenario.frequency');
+        }
+
+        var result = 'NO LABEL';
+        _.each(col,function(v){
+            if (v.code == code){
+                result = v.label;
+                return;
+            }
+        });
+
+        return result;
+    },
+    
+    messageInfo: function(){
+        var m = Messages.findOne(Session.get('scenario.message'));
+        if (m){
+            return m.header;
+        }else{
+            return "";
+        }
+    },
+    
+    hasCoupon: function(){
+        return (Session.get('scenario.coupon'))?true:false;
+    },
+    
+    couponInfo: function(){
+        var c = Coupons.findOne(Session.get('scenario.coupon'));
+        if (c){
+            return c.pass.logoText+" : "+c.pass.coupon.primaryFields[0].value+" "+c.pass.coupon.primaryFields[0].label;
+        }else{
+            return "";
+        }
+    },
+    
+    period: function (){
+        if (Session.get('scenario.start')){
+            if (Session.get('scenario.end')){
+                return 'с '+commonFormatDate.call(this, Session.get('scenario.start'))+' по '+ commonFormatDate.call(this, Session.get('scenario.end'));
+            }else{
+                return 'с '+commonFormatDate.call(this, Session.get('scenario.start'));
+            }
+        }else{
+            if (Session.get('scenario.end')){
+                return 'по '+ commonFormatDate(this, Session.get('scenario.end'));
+            }else{
+                return 'без ограничений по времени';
+            }
+        }
+
+    },
+    
+    isActive: function (){
+        if (Session.get('scenario.active')){
+            return 'Да';
+        }else{
+            return 'Нет';
+        }
+    },
+    
+    frequency: function(){
+        return Session.get('scenario.frequency');
     }
 });
